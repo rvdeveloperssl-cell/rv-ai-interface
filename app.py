2582e8578802846e796133aaa34d1dbc0a21fe19
rvdeveloperssl-cell <rvdevelopers.sl@gmail.com>
2026-03-15 00:08:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -48,7 +48,7 @@ if prompt := st.chat_input("RV AI එකෙන් ඕනෑම දෙයක් �
         
         try:
             # මෙතන 'rv-ai-core' යනු ඔබ දුන් container name එකයි
-            url = "http://172.17.0.1:11434/api/generate"
+            url = "http://65.108.212.204:11434/api/generate"
             payload = {"model": "llama3", "prompt": prompt, "stream": True}
             
             res = requests.post(url, json=payload, stream=True, timeout=10)
